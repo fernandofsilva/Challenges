2b6af26ad18617647ed59a9d3213b51c6bd15745
fernandofsilva <fernandosk8@gmail.com>
2018-10-08 12:15:32 -0300

    Initial Commit

---
+++ Code/School_Budgets_functions.py
@@ -0,0 +1,121 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 13 14:29:09 2018
+
+@author: esssfff
+"""
+
+from warnings import warn
+import numpy as np
+import pandas as pd
+
+# defining log loss function
+def compute_log_loss(predicted, actual, eps=1e-14):
+    """ Computes the logarithmic loss between predicted and actual when these
+    are 10 arrays.
+    
+    :param predicted: The predicted probabilities as floats between 0-1
+    :param actual: The actual binary labels. Either 0 or 1.
+    :param eps (optional): log(0) is inf, so we need to off our predicted
+                           values slightly by eps from 0 or 1.
+    """
+    predicted = np.clip(predicted, eps, 1-eps)
+    loss = (-1 * np.mean(actual * np.log(predicted)) + (1 - actual)
+    * np.log(1 - predicted))
+    
+    return loss
+
+# defining multilable function
+def multilabel_sample(y, size=1000, min_count=5, seed=None):
+    """ Takes a matrix of binary labels `y` and returns
+        the indices for a sample of size `size` if
+        `size` > 1 or `size` * len(y) if size =< 1.
+        The sample is guaranteed to have > `min_count` of
+        each label.
+    """
+    try:
+        if (np.unique(y).astype(int) != np.array([0, 1])).all():
+            raise ValueError()
+    except (TypeError, ValueError):
+        raise ValueError('multilabel_sample only works with binary indicator matrices')
+
+    if (y.sum(axis=0) < min_count).any():
+        raise ValueError('Some classes do not have enough examples. Change min_count if necessary.')
+
+    if size <= 1:
+        size = np.floor(y.shape[0] * size)
+
+    if y.shape[1] * min_count > size:
+        msg = "Size less than number of columns * min_count, returning {} items instead of {}."
+        warn(msg.format(y.shape[1] * min_count, size))
+        size = y.shape[1] * min_count
+
+    rng = np.random.RandomState(seed if seed is not None else np.random.randint(1))
+
+    if isinstance(y, pd.DataFrame):
+        choices = y.index
+        y = y.values
+    else:
+        choices = np.arange(y.shape[0])
+
+    sample_idxs = np.array([], dtype=choices.dtype)
+
+    # first, guarantee > min_count of each label
+    for j in range(y.shape[1]):
+        label_choices = choices[y[:, j] == 1]
+        label_idxs_sampled = rng.choice(label_choices, size=min_count, replace=False)
+        sample_idxs = np.concatenate([label_idxs_sampled, sample_idxs])
+
+    sample_idxs = np.unique(sample_idxs)
+
+    # now that we have at least min_count of each, we can just random sample
+    sample_count = int(size - sample_idxs.shape[0])
+
+    # get sample_count indices from remaining choices
+    remaining_choices = np.setdiff1d(choices, sample_idxs)
+    remaining_sampled = rng.choice(remaining_choices,
+                                   size=sample_count,
+                                   replace=False)
+
+    return np.concatenate([sample_idxs, remaining_sampled])
+
+
+def multilabel_sample_dataframe(df, labels, size, min_count=5, seed=None):
+    """ Takes a dataframe `df` and returns a sample of size `size` where all
+        classes in the binary matrix `labels` are represented at
+        least `min_count` times.
+    """
+    idxs = multilabel_sample(labels, size=size, min_count=min_count, seed=seed)
+    return df.loc[idxs]
+
+
+def multilabel_train_test_split(X, Y, size, min_count=5, seed=None):
+    """ Takes a features matrix `X` and a label matrix `Y` and
+        returns (X_train, X_test, Y_train, Y_test) where all
+        classes in Y are represented at least `min_count` times.
+    """
+    index = Y.index if isinstance(Y, pd.DataFrame) else np.arange(Y.shape[0])
+
+    test_set_idxs = multilabel_sample(Y, size=size, min_count=min_count, seed=seed)
+    train_set_idxs = np.setdiff1d(index, test_set_idxs)
+
+    test_set_mask = index.isin(test_set_idxs)
+    train_set_mask = ~test_set_mask
+
+    return (X[train_set_mask], X[test_set_mask], Y[train_set_mask], Y[test_set_mask])
+
+
+# Define combine_text_columns()
+def combine_text_columns(data_frame, to_drop=NUMERIC_COLUMNS + LABELS):
+    """ converts all text in each row of data_frame to single vector """
+    
+    # Drop non-text columns that are in the df
+    to_drop = set(to_drop) & set(data_frame.columns.tolist())
+    text_data = data_frame.drop(to_drop, axis=1)
+    
+    # Replace nans with blanks
+    text_data.fillna('', inplace=True)
+    
+    # Join all text items in a row that have a space in between
+    return text_data.apply(lambda x: " ".join(x), axis=1)
